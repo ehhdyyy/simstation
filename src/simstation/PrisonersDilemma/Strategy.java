8962c206c45504eac0de50168b6234439939cd8f
Edmond Pan <edmond13pan@gmail.com>
2025-04-16 23:46:59 -0700

    removed dupe pd

--- src/simstation/PrisonersDilemma/Strategy.java
+++
@@ -1,5 +0,0 @@
-package src.simstation.PrisonersDilemma;
-
-public abstract class Strategy {
-    public abstract boolean cooperate(Prisoner prisoner);
-}
--- src/simstation/PrisonersDilemma/Tit4Tat.java
+++
@@ -1,9 +0,0 @@
-package src.simstation.PrisonersDilemma;
-
-public class Tit4Tat extends Strategy{
-    
-    @Override
-    public boolean cooperate(Prisoner prisoner) {
-        return !prisoner.getPartnerCheated(); // Cooperate if the partner did not cheat in the last round, ex: if opponent cheated(true) then return cheat(false aka do not cooperate)
-    }
-}
